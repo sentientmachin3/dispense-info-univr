dd28556c07eb1de8378c6161ab9cf5b017571b90
sentientmachin3 <davideb1912@gmail.com>
2022-10-16 19:48:07 +0200

    Complete build script

--- build.py
+++ build.py
@@ -1,38 +1,102 @@
 #!/usr/bin/env python3
 
 import argparse
+import contextlib
+import glob
+import os
+import shutil
+import subprocess
+import sys
 from pathlib import Path
-from typing import List
+
+BUILD_DIR = Path("./build")
+PROJECT_ROOT = "."
+
+
+@contextlib.contextmanager
+def cd(path):
+    prev_cwd = Path.cwd()
+    os.chdir(path)
+    try:
+        yield
+    finally:
+        os.chdir(prev_cwd)
 
 
 def clean():
-    paths_to_ignore = []
-    with open("path_ignore.txt", "r") as ignore_file:
-        for l in ignore_file:
-            paths_to_ignore.append(Path(l.rstrip()))
+    print("Cleaning up")
+    paths = list(glob.glob("./build/*"))
+    for p in paths:
+        shutil.rmtree(p)
 
-    base_path = Path(".")
-    to_be_deleted: List[Path] = []
-    print("### FILES TO BE SKIPPED ###")
-    for i in base_path.glob("**/*.pdf"):
-        for ignored_path in paths_to_ignore:
-            if (ignored_path in i.parents) or (ignored_path.samefile(i)):
-                print(f"{i.as_posix()}")
-            else:
-                to_be_deleted.append(i)
+    print("Build dir clean")
 
-    print("### FILES TO BE CLEANED UP ###")
-    for deletable in to_be_deleted:
-        print(deletable)
-        # deletable.unlink()
+
+def build_file(path: Path):
+    if path.parts[-1].endswith("tex"):
+        out_path = Path(BUILD_DIR) / path.parent
+        out_path.mkdir(
+            parents=True,
+            exist_ok=True,
+        )
+        full_out_path = out_path.absolute()
+        print(f"Compiling to {out_path.absolute()}")
+        with cd(path.parent):
+            subprocess.check_call(
+                [
+                    "pdflatex",
+                    "-output-directory",
+                    full_out_path,
+                    path.name,
+                ]
+            )
+    else:
+        print("Please provide a file .tex file path")
+        sys.exit(1)
+
+
+def build(path: Path | None):
+    if path is not None and path.exists():
+        # Compile only the given path
+        build_file(path)
+    else:
+        # Compile everything
+        print("Compiling all the files")
+        source_paths = list(Path(PROJECT_ROOT).glob("**/*.tex"))
+        for p in source_paths:
+            build_file(p)
+
+    sys.exit(0)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         "build.py", description="Build and clean script for the repo"
     )
-    parser.add_argument("-c", "--clean", dest="clean", action="store_true")
+    parser.add_argument(
+        "-c",
+        "--clean",
+        dest="clean",
+        action="store_true",
+        help="Delete all pdflatex auxiliary files",
+    )
+    parser.add_argument(
+        "-a",
+        "--build-all",
+        dest="build_all",
+        action="store_true",
+        help="Build everything in the repo",
+    )
+    parser.add_argument("target", nargs="?", default=None, type=Path)
 
     args = vars(parser.parse_args())
-    if "clean" in args.keys():
+    if args["clean"]:
         clean()
+
+    if args["build_all"] and args["target"] is None:
+        print("Building everything")
+        build(None)
+
+    if args["target"] is not None:
+        print(f'Building {args["target"]}')
+        build(args["target"])
